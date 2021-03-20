9619ea91d09254de32612021736754a78e894a9c
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-19 19:28:02 -0700

    add functions to new contact form

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,20 @@ $(document).ready(function(){
   $("#formButton").click(function(){
     $("#form1").toggle();
   });
+  $("#entry-save").submit(function(event){
+    event.preventDefault();
+    
+    const firstNameInput = $("input#firstName").val();
+    const lastNameInput = $("input#lastName").val();
+    const phoneInput = $("input#phoneNumber").val();
+    const emailInput = $("input#emailAddress").val();
+    const homeInput = $("input#homeAddress").val();
+
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".phone").text(phoneInput);
+    $(".email").text(emailInput);
+    $(".home").text(homeInput);
+
+  });
 });
