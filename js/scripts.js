59a3b4a495b6edf81749aeef13138b9052c07849
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-18 15:06:03 -0700

    create Contact constructor that will save form input to a new object and log to console when a user clicks next

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 //back end
-function Contact(firstName,lastName,phoneNumber,email) {
+function Contact(firstName,lastName,phoneNumber,emailAddress) {
   this.firstName = firstName;
   this.lastName = lastName;
+  this.fullName = firstName + " " + lastName;
   this.phoneNumber = phoneNumber;
+  this.emailAddress = emailAddress;
 }
 
 //front end
@@ -11,12 +13,20 @@ $(document).ready(function(){
     $("#form1").toggle();
   });
 
-  $("#next").click(function(){
+  $("#next").click(function(event){
     event.preventDefault();
     $("#confirm-entry").show();
+    let newContact = new Contact($("#firstName").val(),$("#lastName").val(),$("#phone").val(),$("#email").val());
+    console.log(newContact);
   });
 
-  $("#reset").click(function() {
+  $("#confirm-save").click(function(event){
+    event.preventDefault();
+    
+  });
+
+  $("#reset").click(function(event) {
+    event.preventDefault();
     $("#new-saved-contact").toggle();
     $("#confirm-entry").toggle();
   });
