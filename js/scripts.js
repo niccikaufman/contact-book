c8335ffb216ea24de6e5b929bc0c7e133338c66b
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-18 14:27:49 -0700

    initial refactoring with constructors and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+//back end
+function Contact(firstName,lastName,phoneNumber,email) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+//front end
 $(document).ready(function(){
   $("#createNew").click(function(){
     $("#form1").toggle();
@@ -11,24 +19,5 @@ $(document).ready(function(){
   $("#reset").click(function() {
     $("#new-saved-contact").toggle();
     $("#confirm-entry").toggle();
-  });
-
-  $("#confirm-save").click(function(){
-    event.preventDefault();
-    const firstNameInput = $("input#firstName").val();
-    const lastNameInput = $("input#lastName").val();
-    const addressInput = $("input#address").val();
-    const address2Input = $("input#address2").val();
-    const phoneInput = $("input#phone").val();
-    const emailInput = $("input#email").val();
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
-    $(".address").text(addressInput);
-    $(".address2").text(address2Input);
-    $(".phone").text(phoneInput);
-    $(".email").text(emailInput);
-
-    $("#new-saved-contact").show();
   });
 });
